856c6f5cb7079e09aa78b5f7592d9cbdd67ec22d
c.cristallo <c.cristallo6@studenti.uniba.it>
2023-08-14 16:40:54 +0200

    Aggiunto JavaDoc ServerException

--- KmeansClient/src/ServerException.java
+++ KmeansClient/src/ServerException.java
@@ -1,4 +1,11 @@
+/**
+ * Classe che rappresenta un'eccezione personalizzata che viene sollevata quando la connessione al server fallisce.
+ */
 public class ServerException extends Exception {
+    /**
+     * Costruttore di ServeException
+     * @param m stringa che si vuole stampare nel caso venga sollevata.
+     */
     ServerException(String m) {
         super(m);
     }
